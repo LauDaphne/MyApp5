14b50a2d7eb5b8d4f0ed50d9542fa5a1399f1979
LauDaphne <laura.mp.job@gmail.com>
2020-01-14 10:57:18 +0100

    Commit pequeñas mejoras

--- app/src/main/java/com/studium/myapp5/MainActivity.java
+++ app/src/main/java/com/studium/myapp5/MainActivity.java
@@ -3,6 +3,7 @@ package com.studium.myapp5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle(R.string.tittlePrinc);
+    }
+
+    public void crearToast(View view) {
+
+    }
+
+    public void crearNotify(View view) {
     }
 }
